df414bfbee75c5258c3d57261535336bac8858a3
NguyenThaiAnh <thaianhkhtn@gmail.com>
2017-10-26 15:53:14 +0700

    add dependency bootstrap and divide components

--- demo-sotify/src/app/app.module.ts
+++ demo-sotify/src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { ArtistComponent } from './artist/artist.component';
+import { AlbumComponent } from './album/album.component';
+import { SearchComponent } from './search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArtistComponent,
+    AlbumComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule
