d8149c67171fd39df68d11b6f7f606b485846731
NguyenThaiAnh <thaianhkhtn@gmail.com>
2017-11-03 16:46:09 +0700

    get album hot at album component

--- demo-sotify/src/app/album/album.component.ts
+++ demo-sotify/src/app/album/album.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {Subscription} from 'rxjs/Subscription';
 import {SpotifyService} from '../services/spotify.service';
 import {ActivatedRoute, Params, Router} from '@angular/router';
@@ -10,23 +10,33 @@ import {ActivatedRoute, Params, Router} from '@angular/router';
 })
 export class AlbumComponent implements OnInit {
   album: any;
+  albums: any;
   subscription: Subscription;
   key: string;
+  artistId: string[] = ['4AK6F7OLvEQ5QYCBNiQWHq', '6eUKZXaKkcviH0Ku9w2n3V', '6VuMaDnrHyPL1p4EHjYLi7', '3BmGtnKgCSGYIUhmivXKWX', '1KCSPY1glIKqW2TotWuXOR', '4nDoRrQiYLoBzwC5BhVJzF'];
 
   constructor(
     private service: SpotifyService,
     private activedRoute: ActivatedRoute,
-    private router: Router) { }
+    private router: Router) {}
 
   ngOnInit() {
     this.subscription = this.activedRoute.params.subscribe(
       (params: Params) => {
-
-        // get all tracks of album
-        this.service.getTrackByAlbum(params['id']).then((data: any) 	=>  {
-          this.album = data;
-          // console.log(data);
-        });
+        if (params['id']) {
+          // get all tracks of album
+          this.service.getTrackByAlbum(params['id']).then((data: any) => {
+            this.album = data;
+            // console.log(data);
+          });
+        } else {
+          //random album to get album HOT
+          const random = Math.floor(Math.random() * 6);
+          this.service.getAlbumByArtist(this.artistId[random]).then((data: any) 	=>  {
+            console.log(data.items);
+            this.albums = data.items;
+          });
+        }
       }
     );
   }
@@ -35,7 +45,7 @@ export class AlbumComponent implements OnInit {
   // send param key search from nav component to home component
   keySearch(event) {
     this.key = event;
-    this.router.navigate(['/home'], { queryParams: { 'keySearch': this.key } });
+    this.router.navigate(['/home'], {queryParams: {'keySearch': this.key}});
   }
 
 }
